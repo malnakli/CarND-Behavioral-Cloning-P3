d43d4aa15f8eea0e4da687bc310e0c063f23a53a
MoAir <hassoun@outlook.com>
2017-10-21 12:15:40 -0400

    add TensorBorad implementation

--- model.py
+++ model.py
@@ -2,39 +2,49 @@ import pandas as pd
 import cv2
 import numpy as np
 from network_models import Basic, LeNet, NVIDIA
+from keras.callbacks import TensorBoard
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+# NOTE: I am using keras 2.0.8
 
 # ===================================================================== #
-# ========================  Load Training Data ======================== #
+# ========================  split Data ======================== #
 # ===================================================================== #
 
-
-def load_training_data():
-
+# @return pandas DataFrame
+def split_data():
     # NOTE: The reason I am using pandas for even thought is not necessrary,
     # I would like to improve my skill on pandas.
 
     # read  driveing log csv file
     headers = ['center_image', 'left_image', 'right_image',
                'steering', 'throttle', 'break', 'speed']
-    driving_log = pd.read_csv("./data/driving_log.csv",
+    data = pd.read_csv("./data/driving_log.csv",
                               header=None, names=headers)
 
-    # get the relative path of the images
-
+     # get the relative path of the images
     def get_image_path(row):
         for image in headers[:3]:
             row[image] = './' + '/'.join(row[image].split('/')[-3:])
 
         return row
 
-    driving_log = driving_log.apply(get_image_path, axis=1)
+    data = data.apply(get_image_path, axis=1)
 
+
+    return train_test_split(data, test_size=0.2)
+
+# ===================================================================== #
+# ========================  Load Data ======================== #
+# ===================================================================== #
+
+def load_data(samples):
     images = []
     steerings = []
-
-    def get_training_data(row):
+    for index, row in samples.iterrows():        
         # create adjusted steering measurements for the side camera images
-        for header, correction in zip(headers[:3], [0, 0.1, -0.1]):
+        for header, correction in zip(samples.columns[:3], [0, 0.1, -0.1]):
             image = cv2.imread(row[header])
             steering = float(row['steering'])
             images.append(image)
@@ -45,13 +55,18 @@ def load_training_data():
             images.append(image_flipped)
             steerings.append(-(steering + correction))
 
-    driving_log.apply(get_training_data, axis=1)
-
     # convert to numpy array since this what keras required
-    x_train = np.array(images)
-    y_train = np.array(steerings)
+    return shuffle( np.array(images), np.array(steerings))
 
-    return x_train, y_train
+# @samples: panads dataFrame
+def load_data_generator(samples,batch_size=32):
+    num_samples = samples.shape[0]
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            yield load_data(batch_samples)
+
 
 
 # ===================================================================== #
@@ -59,7 +74,7 @@ def load_training_data():
 # ===================================================================== #
 
 
-def run_model(x, y, netModel='Basic'):
+def run_model(fit_kwargs,netModel='Basic' ):
     # look at network model for more details for inside the model architecture
 
     models = {
@@ -72,8 +87,27 @@ def run_model(x, y, netModel='Basic'):
     # http://ruder.io/optimizing-gradient-descent/
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(x, y, batch_size=32, nb_epoch=2,
-              shuffle=True, validation_split=0.2)
+
+    # NOTE: the following is a reference for me, 
+    # Taken from: https://stackoverflow.com/questions/45265436/keras-save-image-embedding-of-the-mnist-data-set
+    # embedding_layer_names = set(layer.name
+    #                         for layer in model.layers
+    #                         if layer.name.startswith('dense'))
+    # embeddings_freq = 1
+
+    #For tensorBoarb
+    # write_graph: Print the graph of neural network as defined internally.
+    # write_images: Create an image by combining the weight of neural network
+    # histogram_freq: Plot the distributions of weights and biases in the neural network
+    # embeddings_freq: frequency (in epochs) at which selected embedding layers will be saved.
+    # embeddings_layer_names: a list of names of layers to keep eye on. If None or empty list all the embedding layer will be watched.
+
+    tbCallback = TensorBoard(log_dir="./graph_logs", histogram_freq=1, 
+    write_graph=True, write_images=True)
+
+    # # https://github.com/fchollet/keras/pull/8023 bug in shuffle argument in keras 2.0.8
+    model.fit_generator(**fit_kwargs , callbacks=[tbCallback])
+    
     save_file_name = str(netModel) + '.h5'
     model.save(save_file_name)
 
@@ -82,9 +116,16 @@ def run_model(x, y, netModel='Basic'):
 # ======================== Execute code ======================== #
 # ===================================================================== #
 
-x_train, y_train = load_training_data()
-print("x_train.shape=", x_train.shape)
-print("y_train.shape=", y_train.shape)
-print('data loaded')
-
-run_model(x_train, y_train, netModel='NVIDIA')
+train , valid = split_data()
+batch_size = 32
+model_fit_generator_arguments = {
+    'generator':load_data_generator(train,batch_size),
+    'steps_per_epoch':int(train.shape[0]/batch_size), 
+    # I would like to use TensorBorad histograms, 
+    # look at this issue https://github.com/fchollet/keras/issues/3358
+    'validation_data':load_data(valid),
+   # 'validation_steps':int(valid.shape[0]/batch_size), # Only relevant if validation_data is a generator 
+   'verbose':1,
+   'epochs':2
+}
+run_model(model_fit_generator_arguments, netModel='Basic')
--- network_models/basic.py
+++ network_models/basic.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
-
+import tensorflow as tf
 
 # @return keras model
 def model():
@@ -8,6 +8,10 @@ def model():
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
     model.add(Cropping2D(cropping=((65, 20), (0, 0))))
     model.add(Flatten())
-    model.add(Dense(1))
+    # it is used for tensorBorad
+    with tf.name_scope('connect_layers'):
+        model.add(Dense(128))
+        model.add(Dense(64))
+        model.add(Dense(1))
 
     return model
